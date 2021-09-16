d8eb6c2db6164016a5269a003d2d0f8017e3fb15
areddin409 <areddin409@gmail.com>
2021-09-16 16:10:54 -0500

    removing config

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+//ALert
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
