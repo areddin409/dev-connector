7b9b11f3c49c1b1131b8281d35fab33dc19f08f2
Andrew Reddin <areddin409@gmail.com>
2021-09-16 20:45:54 -0500

    Created register action and reducer

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -22,27 +23,7 @@ const Register = ({ setAlert }) => {
     if (password !== passwordConfirm) {
       setAlert('Passwords do not match', 'danger', 3000);
     } else {
-      console.log('Success');
-      // const newUser = {
-      //   name,
-      //   email,
-      //   password,
-      //   passwordConfirm
-      // };
-      // try {
-      //   const config = {
-      //     headers: {
-      //       'Content-Type': 'application/json'
-      //     }
-      //   };
-
-      //   const body = JSON.stringify(newUser);
-
-      //   const res = await axios.post('/api/users', body, config);
-      //   console.log(res.data);
-      // } catch (error) {
-      //   console.error(error.response.data);
-      // }
+      register({ name, email, password });
     }
   };
 
@@ -111,7 +92,11 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, {
+  setAlert,
+  register
+})(Register);
